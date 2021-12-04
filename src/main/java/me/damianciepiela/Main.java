370b4f8e5d1fd190c44ecebfaf797984c5d34be0
Damian Ciepiela <dciepiela@yahoo.com>
2021-12-04 20:28:05 +0100

    fix: handle running with 0 arguments

--- src/main/java/me/damianciepiela/Main.java
+++ src/main/java/me/damianciepiela/Main.java
@@ -8,6 +8,7 @@ import java.io.IOException;
 public class Main {
 
     public static void main(String[] args) {
+        if (args.length == 0) return;
         if(args[0].equals("Client")) {
             try {
                 LoggerAdapter loggerAdapter = new LoggerAdapter(Client.class);
