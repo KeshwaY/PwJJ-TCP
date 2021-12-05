a68d87f35904c087c158ef0d759ca93023a9bf94
Damian Ciepiela <dciepiela@yahoo.com>
2021-12-05 13:21:54 +0100

    feat: add methods to Connection to check if connection between sources is active

--- src/main/java/me/damianciepiela/Connection.java
+++ src/main/java/me/damianciepiela/Connection.java
@@ -12,4 +12,19 @@ public interface Connection {
     static String getFromSource(DataInputStream dataInputStream) throws IOException {
         return dataInputStream.readUTF();
     }
+
+    static boolean checkIfSourceIsActive(DataOutputStream dataOutputStream, DataInputStream dataInputStream) throws IOException {
+        sendToSource(dataOutputStream, "ping");
+        String fromSource = getFromSource(dataInputStream);
+        return fromSource.equals("pong");
+    }
+
+    static boolean refreshConnection(DataOutputStream dataOutputStream, DataInputStream dataInputStream) throws IOException {
+        String fromSource = getFromSource(dataInputStream);
+        if (fromSource.equals("ping")) {
+            sendToSource(dataOutputStream, "pong");
+            return true;
+        }
+        return false;
+    }
 }
