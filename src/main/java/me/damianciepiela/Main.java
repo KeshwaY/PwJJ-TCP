f0b4c9d1f594d91e65cd61f6e96d07c48697bb4e
Damian Ciepiela <dciepiela@yahoo.com>
2021-12-05 09:49:20 +0100

    feat: add shutdownHook to main refactor: code cleanup in main

--- src/main/java/me/damianciepiela/Main.java
+++ src/main/java/me/damianciepiela/Main.java
@@ -7,25 +7,53 @@ import java.io.IOException;
 
 public class Main {
 
+    static void client(String address, int port) {
+        LoggerAdapter loggerAdapter = createLogger(Client.class);
+        try {
+            Client client = new Client(address, port, loggerAdapter);
+            client.test();
+            shutDownHook(client, loggerAdapter);
+        } catch (IOException e) {
+            loggerAdapter.error(e);
+        }
+    }
+
+     static void server(int port, int capacity) {
+         LoggerAdapter loggerAdapter = createLogger(Server.class);
+         try {
+             Server server = new Server(port, loggerAdapter, capacity);
+             server.start();
+             shutDownHook(server, loggerAdapter);
+        } catch (IOException e) {
+            loggerAdapter.error(e);
+         }
+     }
+
+     static LoggerAdapter createLogger(Class<?> clazz) {
+        return new LoggerAdapter(clazz);
+     }
+
+     static void shutDownHook(Closable closable, LoggerAdapter logger) {
+         Runtime.getRuntime().addShutdownHook(new Thread(() -> {
+             try {
+                 logger.info(logger.getName() + "is shutting down...");
+                 closable.close();
+             } catch (IOException e) {
+                 e.printStackTrace();
+             }
+         }));
+     }
+
     public static void main(String[] args) {
         if (args.length == 0) return;
+
         if(args[0].equals("Client")) {
-            LoggerAdapter loggerAdapter = new LoggerAdapter(Client.class);
-            try {
-                Client client = new Client("localhost", 887, loggerAdapter);
-                client.test();
-            } catch (IOException e) {
-                loggerAdapter.error(e);
-            }
+            client("localhost", 887);
         }
+
         if(args[0].equals("Server")) {
-            LoggerAdapter loggerAdapter = new LoggerAdapter(Server.class);
-            try {
-                Server server = new Server(887, loggerAdapter);
-                server.start();
-            } catch (IOException e) {
-                loggerAdapter.error(e);
-            }
+            int capacity = Integer.parseInt(args[1]);
+            server(887, capacity);
         }
     }
 
