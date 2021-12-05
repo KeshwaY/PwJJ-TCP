561af327f8b848e53733777f1d3d8b976bd1abe5
Damian Ciepiela <dciepiela@yahoo.com>
2021-12-05 13:22:51 +0100

    chore: rename main method feat: client now sends student's indentity to the server fix: logging calls

--- src/main/java/me/damianciepiela/client/Client.java
+++ src/main/java/me/damianciepiela/client/Client.java
@@ -6,10 +6,13 @@ import me.damianciepiela.LoggerAdapter;
 
 import java.io.*;
 import java.net.Socket;
+import java.util.Scanner;
 
 public class Client implements Connection, Closable {
     private final LoggerAdapter logger;
     private final Socket socket;
+
+    private boolean connected;
 
     private final DataInputStream inFromServer;
     private final DataOutputStream outToServer;
@@ -19,24 +22,54 @@ public class Client implements Connection, Closable {
         this.socket = new Socket(address, port);
         this.inFromServer = new DataInputStream(socket.getInputStream());
         this.outToServer = new DataOutputStream(socket.getOutputStream());
-        this.logger.debug("Client created.");
+        this.connected = true;
+        this.logger.debug("Connection established");
+        this.logger.info("Client created.");
     }
 
-    public void test() throws IOException {
+    public void setIdentity() throws IOException {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Podaj numer albumu: ");
+        int id = scanner.nextInt();
+        System.out.println("Podaj imie: ");
+        String name = scanner.next();
+        System.out.println("Podaj nazwisko: ");
+        String surname = scanner.next();
+
         try {
-            sendTo("TEST");
-            System.out.println(getFrom());
-        } catch (ClassNotFoundException e) {
-            e.printStackTrace();
+            sendTo(String.valueOf(id));
+            sendTo(name);
+            sendTo(surname);
+        } catch (IOException e) {
+            close();
         }
     }
 
-    public String getFrom() throws IOException, ClassNotFoundException {
-        return Connection.getFromSource(this.inFromServer);
+    public void start() {
+        try {
+            setIdentity();
+            while(connected) {
+                checkConnection();
+            }
+        } catch (IOException e) {
+            this.logger.error(e);
+        }
+    }
+
+    public String getFrom() throws IOException {
+        String fromServer = Connection.getFromSource(this.inFromServer);
+        this.logger.debug("Got content from Server on " + this.socket.getInetAddress() + ": " + fromServer);
+        return fromServer;
     }
 
     public void sendTo(String text) throws IOException {
+        this.logger.debug("Sending to content to Server on " + this.socket.getInetAddress() + ": " + text);
         Connection.sendToSource(this.outToServer, text);
+    }
+
+    public void checkConnection() throws IOException {
+        this.connected = Connection.refreshConnection(this.outToServer, this.inFromServer);
+        //this.logger.debug("Server on " + this.socket.getInetAddress() + " connection status: " + this.connected);
     }
 
     public void close() throws IOException {
