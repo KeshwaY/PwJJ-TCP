d9201ddd8e1db24846cd70a5a424e2901d03f784
Damian Ciepiela <dciepiela@yahoo.com>
2021-12-12 14:46:15 +0100

    fix: Option to disable loging in LoggerAdapter

--- src/main/java/me/damianciepiela/LoggerAdapter.java
+++ src/main/java/me/damianciepiela/LoggerAdapter.java
@@ -1,7 +1,9 @@
 package me.damianciepiela;
 
+import org.apache.logging.log4j.Level;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import org.apache.logging.log4j.core.config.Configurator;
 
 public class LoggerAdapter {
     private final Logger logger;
@@ -26,6 +28,8 @@ public class LoggerAdapter {
         this.logger.error(toLog);
     }
 
-
+    public void changeLevel(Level level) {
+        Configurator.setLevel(this.logger.getName(), level);
+    }
 
 }
